4cb864cc619f891442ae527ae48ea3e07ee92442
Snejana <snezhanapirdopska@gmail.com>
2021-11-07 18:20:06 +0200

    Initial commit

---
+++ Lec4.py
@@ -0,0 +1,3 @@
+x=5
+if x==5: print("thd",x)
+elif x>3: print("yey")
---
+++ Zad7.py
@@ -0,0 +1,9 @@
+x=10
+if 1 <= x <= 3:
+    print(x*10)
+elif 4 <= x <= 6:
+    print(x * 100)
+elif 7 <= x <= 9:
+    print(x * 1000)
+else:
+    print("Error")
