99ed9d6218d71d8f32a1a9063a5671ea92a04456
Snejana <snezhanapirdopska@gmail.com>
2021-11-30 23:12:35 +0200

    Initial commit

--- Lecture8/Zad7.py
+++ Lecture8/Zad7.py
@@ -1,13 +1,13 @@
-initial_list = [2,2, 1, 1, 2, 3, 3, 2,2,2,2,1]
-new_lst = [0,0,initial_list[0]]
+initial_list = [2,1, 1, 2, 3, 3, 2,2,2,2,1]
+new_lst = []
 max_list = []
 for item in range(1, len(initial_list)):
     if initial_list[item] == initial_list[item-1]:
-
         new_lst.append(initial_list[item])
         if len(new_lst) > len(max_list):
             max_list = new_lst
+            max_list.append(initial_list[item])
     else:
-        new_lst = [0,0,initial_list[item]]
+        new_lst = []
 
-print(max_list[2:])
+print(max_list)
