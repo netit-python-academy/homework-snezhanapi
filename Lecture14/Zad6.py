b114c265f455ccfdc572d547a44a86d4b03bcf30
Snejana <snezhanapirdopska@gmail.com>
2021-12-11 22:31:47 +0200

    Initial commit

---
+++ Lecture14/Zad6.py
@@ -0,0 +1,2 @@
+list_tuples = [(1,3,5), (8,2,4), (3,6,1)]
+print(sorted(list_tuples,key=lambda x: x[0]))
---
+++ Lecture14/Zad7.py
@@ -0,0 +1,4 @@
+list_dict = [{ "name" : "Nandini", "age" : 20},
+{ "name" : "Manjeet", "age" : 20 },
+{ "name" : "Nikhil" , "age" : 19 }]
+print(sorted(list_dict,key=lambda x: x["age"]))
