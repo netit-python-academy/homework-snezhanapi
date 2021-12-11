3e45357d3a644965faec1284b0e76e5176e35c55
Snejana <snezhanapirdopska@gmail.com>
2021-12-11 20:22:10 +0200

    Initial commit

---
+++ Lecture14/Zad1.py
@@ -0,0 +1,8 @@
+def find_element_in_list(element, list_element):
+    try:
+        index_element = list_element.index(element)
+        return index_element
+    except ValueError:
+        return None
+list = [1,2,3,4,5]
+print(find_element_in_list(3,list))
