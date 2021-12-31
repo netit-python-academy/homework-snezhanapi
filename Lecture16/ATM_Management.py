2a414205307c7ee1c4828cf3f631e20ad7de9a7e
Snejana <snezhanapirdopska@gmail.com>
2021-12-31 23:43:19 +0200

    Initial commit

--- Lecture16/ATM_Management.py
+++ Lecture16/ATM_Management.py
@@ -3,11 +3,12 @@ from Project_ATM_Database import customer_pin
 
 
 def validate_pin(cust_id):
-    for i in range(0, 2):
+    for i in range(0, 3):
         input_pin = input("Please enter your pin\n")
-        if input_pin == customer_pin[cust_id]:
+        if int(input_pin) == customer_pin[cust_id]:
             x = True
             break
         else:
+            print("Invalid pin\n")
             x = False
     return x
--- Lecture16/Project_ATM.py
+++ Lecture16/Project_ATM.py
@@ -4,8 +4,11 @@ from ATM_Management import validate_pin
 from Project_ATM_Database import customer_id
 customer_name = input("Please enter your name\n")
 if customer_name in customer_id.keys():
-    print(customer_id[customer_name])
-    is_pin_valid = validate_pin()
-    if
+    customer_id_number = customer_id[customer_name]
+    is_pin_valid = validate_pin(customer_id_number)
+    if is_pin_valid:
+        print('Valid pin')
+    else:
+        print("Your card has been blocked")
 else:
     print("Invalid customer name")
